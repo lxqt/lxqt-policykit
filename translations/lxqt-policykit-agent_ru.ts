9648ca19312de511946883b404e5d7131e8d5f3a
Ilya87 <yast4ik@yahoo.com>
2014-10-28 22:42:41 +0300

    Add Russian translation

---
+++ translations/lxqt-policykit-agent_ru.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>LxQtPolicykit::PolicykitAgent</name>
+    <message>
+        <source>PolicyKit Information</source>
+        <translation>Информация PolicyKit</translation>
+    </message>
+    <message>
+        <source>Authorization Failed</source>
+        <translation>Авторизация не удалась</translation>
+    </message>
+    <message>
+        <source>Authorization failed for some reason</source>
+        <translation>Авторизация не удалась по какой-то причине</translation>
+    </message>
+    <message>
+        <source>PolicyKit Error</source>
+        <translation>Ошибка PolicyKit</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <source>Authentication Required</source>
+        <translation>Требуется аутентификация</translation>
+    </message>
+    <message>
+        <source>Identity:</source>
+        <translation>Идентификация:</translation>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation>Пароль:</translation>
+    </message>
+</context>
+</TS>
