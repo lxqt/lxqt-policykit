19f9cabde22b1033e9eef6997fb301341a86957e
Sérgio Marques <smarquespt@gmail.com>
2015-01-07 13:16:14 +0000

    Portuguese update

---
+++ translations/lxqt-policykit-agent_pt.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_PT">
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <source>Authentication Required</source>
+        <translation>Requer autenticação</translation>
+    </message>
+    <message>
+        <source>Identity:</source>
+        <translation>Identidade:</translation>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation>Senha:</translation>
+    </message>
+</context>
+<context>
+    <name>LxQtPolicykit::PolicykitAgent</name>
+    <message>
+        <source>PolicyKit Information</source>
+        <translation>Informaçoes PolicyKit</translation>
+    </message>
+    <message>
+        <source>Authorization Failed</source>
+        <translation>Falha de autenticação</translation>
+    </message>
+    <message>
+        <source>Authorization failed for some reason</source>
+        <translation>A autenticação falhou por alguma razão</translation>
+    </message>
+    <message>
+        <source>PolicyKit Error</source>
+        <translation>Erro PolicyKit</translation>
+    </message>
+</context>
+</TS>
