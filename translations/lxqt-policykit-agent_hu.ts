d400f4f533eed3d0449a3feac5b6962ba8807478
attus <attus@enterpriseforever.com>
2015-05-20 14:56:40 +0200

    Hungarian translation

---
+++ translations/lxqt-policykit-agent_hu.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <source>Authentication Required</source>
+        <translation>Hitelesítés kell</translation>
+    </message>
+    <message>
+        <source>Identity:</source>
+        <translation>Név:</translation>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation>Jelszó:</translation>
+    </message>
+</context>
+<context>
+    <name>LxQtPolicykit::PolicykitAgent</name>
+    <message>
+        <source>PolicyKit Information</source>
+        <translation>Policykit információ</translation>
+    </message>
+    <message>
+        <source>Authorization Failed</source>
+        <translation>Hitelesítés sikertelen</translation>
+    </message>
+    <message>
+        <source>Authorization failed for some reason</source>
+        <translation>Hitelesítés valami miatt sikertelen</translation>
+    </message>
+    <message>
+        <source>PolicyKit Error</source>
+        <translation>Policykit hiba</translation>
+    </message>
+</context>
+</TS>
