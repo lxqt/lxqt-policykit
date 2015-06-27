e47dda6d38a0d58a057a054e259cb3e9649c6799
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-06-27 23:09:14 +0200

    Added missing tr() call.

--- translations/lxqt-policykit-agent.ts
+++ translations/lxqt-policykit-agent.ts
@@ -1,37 +1,50 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
-<context>
-    <name>PolicykitAgentGUI</name>
-    <message>
-        <source>Authentication Required</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Identity:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Password:</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
+<TS version="2.1">
 <context>
     <name>LxQtPolicykit::PolicykitAgent</name>
     <message>
+        <location filename="../src/policykitagent.cpp" line="71"/>
+        <location filename="../src/policykitagent.cpp" line="149"/>
         <source>PolicyKit Information</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/policykitagent.cpp" line="71"/>
+        <source>Another authentization in progress. Please try it again later.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="132"/>
         <source>Authorization Failed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/policykitagent.cpp" line="132"/>
         <source>Authorization failed for some reason</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/policykitagent.cpp" line="144"/>
         <source>PolicyKit Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
