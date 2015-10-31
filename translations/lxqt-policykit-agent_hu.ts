bd0690f698a4adb0dc10d68c156ed42b60cd5881
Jerome Leclanche <jerome@leclan.ch>
2015-10-31 02:25:38 +0200

    Update translations

--- translations/lxqt-policykit-agent_hu.ts
+++ translations/lxqt-policykit-agent_hu.ts
@@ -1,38 +1,51 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
-<context>
-    <name>PolicykitAgentGUI</name>
-    <message>
-        <source>Authentication Required</source>
-        <translation>Hitelesítés kell</translation>
-    </message>
-    <message>
-        <source>Identity:</source>
-        <translation>Név:</translation>
-    </message>
-    <message>
-        <source>Password:</source>
-        <translation>Jelszó:</translation>
-    </message>
-</context>
+<TS version="2.1" language="en_US">
 <context>
     <name>LXQtPolicykit::PolicykitAgent</name>
     <message>
+        <location filename="../src/policykitagent.cpp" line="71"/>
+        <location filename="../src/policykitagent.cpp" line="149"/>
         <source>PolicyKit Information</source>
         <translation>Policykit információ</translation>
     </message>
     <message>
+        <location filename="../src/policykitagent.cpp" line="71"/>
+        <source>Another authentization in progress. Please try it again later.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="132"/>
         <source>Authorization Failed</source>
         <translation>Hitelesítés sikertelen</translation>
     </message>
     <message>
+        <location filename="../src/policykitagent.cpp" line="132"/>
         <source>Authorization failed for some reason</source>
         <translation>Hitelesítés valami miatt sikertelen</translation>
     </message>
     <message>
+        <location filename="../src/policykitagent.cpp" line="144"/>
         <source>PolicyKit Error</source>
         <translation>Policykit hiba</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>Hitelesítés kell</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>Név:</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>Jelszó:</translation>
     </message>
 </context>
 </TS>
--- translations/lxqt-policykit-agent_pt.ts
+++ translations/lxqt-policykit-agent_pt.ts
@@ -1,38 +1,51 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="pt">
-<context>
-    <name>PolicykitAgentGUI</name>
-    <message>
-        <source>Authentication Required</source>
-        <translation>Requer autenticação</translation>
-    </message>
-    <message>
-        <source>Identity:</source>
-        <translation>Identidade:</translation>
-    </message>
-    <message>
-        <source>Password:</source>
-        <translation>Senha:</translation>
-    </message>
-</context>
+<TS version="2.1" language="pt">
 <context>
     <name>LXQtPolicykit::PolicykitAgent</name>
     <message>
+        <location filename="../src/policykitagent.cpp" line="71"/>
+        <location filename="../src/policykitagent.cpp" line="149"/>
         <source>PolicyKit Information</source>
         <translation>Informaçoes PolicyKit</translation>
     </message>
     <message>
+        <location filename="../src/policykitagent.cpp" line="71"/>
+        <source>Another authentization in progress. Please try it again later.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="132"/>
         <source>Authorization Failed</source>
         <translation>Falha de autenticação</translation>
     </message>
     <message>
+        <location filename="../src/policykitagent.cpp" line="132"/>
         <source>Authorization failed for some reason</source>
         <translation>A autenticação falhou por alguma razão</translation>
     </message>
     <message>
+        <location filename="../src/policykitagent.cpp" line="144"/>
         <source>PolicyKit Error</source>
         <translation>Erro PolicyKit</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>Requer autenticação</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>Identidade:</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>Senha:</translation>
     </message>
 </context>
 </TS>
