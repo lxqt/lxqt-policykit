a4df87586834a3c2711499249555ec9c69e9d749
safaalfulaij <safa1996alfulaij@gmail.com>
2016-07-08 17:33:08 +0300

    Update Arabic translations

---
+++ lxqt-policykit-agent_ar.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="71"/>
+        <location filename="../../../src/policykitagent.cpp" line="149"/>
+        <source>PolicyKit Information</source>
+        <translation>معلومات عدّة السّياسات</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="71"/>
+        <source>Another authentization in progress. Please try it again later.</source>
+        <translation>تجري عمليّة استيثاق أخرى حاليًّا. فضلًا جرّب مجدّدًا لاحقًا.</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="132"/>
+        <source>Authorization Failed</source>
+        <translation>فشل الاستيثاق</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="132"/>
+        <source>Authorization failed for some reason</source>
+        <translation>فشل الاستيثاق لسبب ما</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="144"/>
+        <source>PolicyKit Error</source>
+        <translation>خطأ في عدّة السّياسات</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>الاستيثاق مطلوب</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>الهويّة:</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>كلمة المرور:</translation>
+    </message>
+</context>
+</TS>
