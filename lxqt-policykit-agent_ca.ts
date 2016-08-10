74abce451e9fa21ebfbe3fd99b2f24b274797721
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-10 23:37:40 +0200

    Add lxqt-policykit-agent_ca.ts (Catalan)

---
+++ lxqt-policykit-agent_ca.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="71"/>
+        <location filename="../../../src/policykitagent.cpp" line="149"/>
+        <source>PolicyKit Information</source>
+        <translation>Informació de PolicyKit</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="71"/>
+        <source>Another authentization in progress. Please try it again later.</source>
+        <translation>Un altre procés d&apos;autenticació en progrés. Proveu-ho de nou més tard.</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="132"/>
+        <source>Authorization Failed</source>
+        <translation>Ha fallat l&apos;autenticació</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="132"/>
+        <source>Authorization failed for some reason</source>
+        <translation>L&apos;autorització ha fallat per algun motiu</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="144"/>
+        <source>PolicyKit Error</source>
+        <translation>Error de PolicyKit</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>Autenticació requerida</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>Identitat:</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>Contrasenya:</translation>
+    </message>
+</context>
+</TS>
