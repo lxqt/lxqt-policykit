1e4556e0bdb701118b1c04eb101274f97b519e67
Pavel Fric <pavelfric@yahoo.com>
2016-08-16 06:54:50 +0200

    Add/Update Czech translations

---
+++ lxqt-policykit-agent_cs.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="71"/>
+        <location filename="../../../src/policykitagent.cpp" line="149"/>
+        <source>PolicyKit Information</source>
+        <translation>Informace PolicyKit</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="71"/>
+        <source>Another authentization in progress. Please try it again later.</source>
+        <translation>Probídá ještě jiná autentizace. Zkuste to, prosím, později znovu.</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="132"/>
+        <source>Authorization Failed</source>
+        <translation>Autorizace se nezdařila</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="132"/>
+        <source>Authorization failed for some reason</source>
+        <translation>Autorizace se z nějakého důvodu nepodařila</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="144"/>
+        <source>PolicyKit Error</source>
+        <translation>Chyba PolicyKit</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>Požadována autentizace</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>Totožnost:</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>Heslo:</translation>
+    </message>
+</context>
+</TS>
