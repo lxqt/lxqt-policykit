6f69da85f3d99bc27ed76fc18649ef5e35f855c8
knedlyk <yupadmin@gmail.com>
2016-11-02 23:29:38 +0100

    Add/update (complete) Ukrainian translation
    * Initial translate of libfm-qt
    * libfm-qt Ukrainian translation update
    * Initial translation of compton-conf
    * compton-conf Ukrainian translation
    * Updated Ukrainian translation #1
    * Updated Ukrainian translation #2
    * Updated Ukrainian translation #3
    * Updated Ukrainian translation #4
    * Updated Ukrainian translation #5
    * Panel's plugins Ukrainian translation completed
    * Ukrainian translation update #6
    * Ukrainian translation update #7
    * Ukrainian translation update #8

---
+++ lxqt-policykit-agent_uk.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="71"/>
+        <location filename="../../../src/policykitagent.cpp" line="149"/>
+        <source>PolicyKit Information</source>
+        <translation>Інформація про PolicyKit</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="71"/>
+        <source>Another authentization in progress. Please try it again later.</source>
+        <translation>Запущено інший процес розпізнавання. Спробуйте пізніше.</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="132"/>
+        <source>Authorization Failed</source>
+        <translation>Розпізнавання не вдалося</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="132"/>
+        <source>Authorization failed for some reason</source>
+        <translation>Чомусь розпізнавання не вдалося</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="144"/>
+        <source>PolicyKit Error</source>
+        <translation>Помилка PolicyKit</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>Вимагається розпізнавання</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation type="unfinished">Ідентифікатор:</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>Пароль:</translation>
+    </message>
+</context>
+</TS>
