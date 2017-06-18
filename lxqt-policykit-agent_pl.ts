84e22fda5f12c23ffa49d553d1576caa1efc9f17
Marcin Mikołajczak <me@m4sk.in>
2017-06-18 20:02:30 +0200

    Updated Polish translation
    Signed-off-by: Marcin Mikołajczak <me@m4sk.in>

---
+++ lxqt-policykit-agent_pl.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="71"/>
+        <location filename="../../../src/policykitagent.cpp" line="149"/>
+        <source>PolicyKit Information</source>
+        <translation>Informacje kontroli przywilejów PolicyKit</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="71"/>
+        <source>Another authentization in progress. Please try it again later.</source>
+        <translation>Jesteś w trakcie innego uwierzytelniania. Spróbuj ponownie.</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="132"/>
+        <source>Authorization Failed</source>
+        <translation>Uwierzytelnienie nie powiodło się</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="132"/>
+        <source>Authorization failed for some reason</source>
+        <translation>Uwierzytelnienie nie powiodło się</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="144"/>
+        <source>PolicyKit Error</source>
+        <translation>Błąd PolicyKit</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>Wymagane jest uwierzytelnienie</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>Tożsamość:</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>Hasło:</translation>
+    </message>
+</context>
+</TS>
