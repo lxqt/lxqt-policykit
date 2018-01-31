31838dd82c8b15f74f35f229bbcadc3a411e4f85
notname000 <notname000@outlook.com>
2018-01-31 13:52:07 +0800

    Update zh_CN translations

---
+++ lxqt-policykit-agent_zh_CN.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="71"/>
+        <location filename="../../../src/policykitagent.cpp" line="158"/>
+        <source>PolicyKit Information</source>
+        <translation>PolicyKit 信息</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="71"/>
+        <source>Another authentization in progress. Please try it again later.</source>
+        <translation>另外的认证正在进行，请稍后再试。</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="139"/>
+        <source>Authorization Failed</source>
+        <translation>认证失败</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="139"/>
+        <source>Authorization failed for some reason</source>
+        <translation>认证因不明原因失败</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="153"/>
+        <source>PolicyKit Error</source>
+        <translation>PolicyKit 错误</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>需要认证</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>ID:</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>密码:</translation>
+    </message>
+</context>
+</TS>
