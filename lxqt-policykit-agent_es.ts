ae1440e8ab0291140ddec8e84ae0df4c58e32aa8
yo <yo@ejemplo.com>
2018-04-28 16:01:42 +0200

    Complete Spanish translation

---
+++ lxqt-policykit-agent_es.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES" sourcelanguage="es">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="71"/>
+        <location filename="../../../src/policykitagent.cpp" line="158"/>
+        <source>PolicyKit Information</source>
+        <translation>Información de PolicyKit</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="71"/>
+        <source>Another authentization in progress. Please try it again later.</source>
+        <translation>Otra autenticación está en curso. Inténtelo de nuevo más tarde.</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="139"/>
+        <source>Authorization Failed</source>
+        <translation>La autorización ha fallado</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="139"/>
+        <source>Authorization failed for some reason</source>
+        <translation>La autorización ha fallado por alguna razón</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagent.cpp" line="153"/>
+        <source>PolicyKit Error</source>
+        <translation>Error de PolicyKit</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>Autenticación requerida</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>Identidad:</translation>
+    </message>
+    <message>
+        <location filename="../../../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>Contraseña:</translation>
+    </message>
+</context>
+</TS>
