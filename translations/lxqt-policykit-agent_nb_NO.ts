5318160de3c4ce7bbcf4ab1e988cbe97b14516ca
Einar Mostad <einar@mostad.eu>
2018-11-24 23:46:11 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Policykit (Agent)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-policykit-agent/nb_NO/

--- translations/lxqt-policykit-agent_nb_NO.ts
+++ translations/lxqt-policykit-agent_nb_NO.ts
@@ -7,27 +7,27 @@
         <location filename="../src/policykitagent.cpp" line="71"/>
         <location filename="../src/policykitagent.cpp" line="158"/>
         <source>PolicyKit Information</source>
-        <translation></translation>
+        <translation>PolicyKit-informasjon</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="71"/>
         <source>Another authentization in progress. Please try it again later.</source>
-        <translation></translation>
+        <translation>En annen autentisering foregår. Prøv igjen senere.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="139"/>
         <source>Authorization Failed</source>
-        <translation></translation>
+        <translation>Autentisering feilet</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="139"/>
         <source>Authorization failed for some reason</source>
-        <translation></translation>
+        <translation>Autentisering feilet av ukjent grunn</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="153"/>
         <source>PolicyKit Error</source>
-        <translation></translation>
+        <translation>PolicyKit-feil</translation>
     </message>
 </context>
 <context>
@@ -35,17 +35,17 @@
     <message>
         <location filename="../src/policykitagentgui.ui" line="14"/>
         <source>Authentication Required</source>
-        <translation></translation>
+        <translation>Autentisering kreves</translation>
     </message>
     <message>
         <location filename="../src/policykitagentgui.ui" line="55"/>
         <source>Identity:</source>
-        <translation></translation>
+        <translation>Identitet:</translation>
     </message>
     <message>
         <location filename="../src/policykitagentgui.ui" line="65"/>
         <source>Password:</source>
-        <translation></translation>
+        <translation>Passord:</translation>
     </message>
 </context>
 </TS>
