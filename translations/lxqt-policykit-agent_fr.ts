958235f48c1e9bcb25707327749d8054edd3dc32
roxfr <roxfr@outlook.fr>
2019-02-25 09:52:31 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Policykit (Agent)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-policykit-agent/fr/

--- translations/lxqt-policykit-agent_fr.ts
+++ translations/lxqt-policykit-agent_fr.ts
@@ -7,27 +7,27 @@
         <location filename="../src/policykitagent.cpp" line="71"/>
         <location filename="../src/policykitagent.cpp" line="158"/>
         <source>PolicyKit Information</source>
-        <translation>Information PolicyKit</translation>
+        <translation>Information sur PolicyKit</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="71"/>
         <source>Another authentization in progress. Please try it again later.</source>
-        <translation>Une autre authentification est en cours. Essayez un peu plus tard SVP.</translation>
+        <translation>Une autre authentification est en cours. Veuillez réessayer plus tard.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="139"/>
         <source>Authorization Failed</source>
-        <translation>Echec de l&apos;autorisation</translation>
+        <translation>Échec de l&apos;autorisation</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="139"/>
         <source>Authorization failed for some reason</source>
-        <translation>Echec de l&apos;autorisation pour une raison donnée</translation>
+        <translation>L&apos;autorisation a échoué pour une raison quelconque</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="153"/>
         <source>PolicyKit Error</source>
-        <translation>Erreur PolicyKit</translation>
+        <translation>Erreur de PolicyKit</translation>
     </message>
 </context>
 <context>
@@ -35,7 +35,7 @@
     <message>
         <location filename="../src/policykitagentgui.ui" line="14"/>
         <source>Authentication Required</source>
-        <translation>Authentification est nécessaire</translation>
+        <translation>Authentification requise</translation>
     </message>
     <message>
         <location filename="../src/policykitagentgui.ui" line="55"/>
