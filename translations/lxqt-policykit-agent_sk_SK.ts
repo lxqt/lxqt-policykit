123c2e046bbb7011fd7ae02c58c079f267063142
Paolo Vigoroso <xpaolox@zoznam.sk>
2019-05-28 09:30:25 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Policykit (Agent)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-policykit-agent/sk_SK/

--- translations/lxqt-policykit-agent_sk_SK.ts
+++ translations/lxqt-policykit-agent_sk_SK.ts
@@ -7,27 +7,27 @@
         <location filename="../src/policykitagent.cpp" line="71"/>
         <location filename="../src/policykitagent.cpp" line="158"/>
         <source>PolicyKit Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Informácie PolicyKit</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="71"/>
         <source>Another authentization in progress. Please try it again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Zaneprázdnené iným, práve prebiehajúcom overovaním totožnosti. Skúste znovu neskôr.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="139"/>
         <source>Authorization Failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Zlyhalo udelenie oprávnenia</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="139"/>
         <source>Authorization failed for some reason</source>
-        <translation type="unfinished"></translation>
+        <translation>Udelenie oprávnenie z nejakého dôvodu zlyhalo</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="153"/>
         <source>PolicyKit Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Chyba PolicyKit</translation>
     </message>
 </context>
 <context>
@@ -35,17 +35,17 @@
     <message>
         <location filename="../src/policykitagentgui.ui" line="14"/>
         <source>Authentication Required</source>
-        <translation type="unfinished"></translation>
+        <translation>Vyžaduje sa overenie totožnosti</translation>
     </message>
     <message>
         <location filename="../src/policykitagentgui.ui" line="55"/>
         <source>Identity:</source>
-        <translation type="unfinished"></translation>
+        <translation>Totožnosť:</translation>
     </message>
     <message>
         <location filename="../src/policykitagentgui.ui" line="65"/>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Heslo:</translation>
     </message>
 </context>
 </TS>
