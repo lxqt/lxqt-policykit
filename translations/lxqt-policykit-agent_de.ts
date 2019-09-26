27c7ed1d51890df0407c1b064295710e00f1ca52
hugo G <hugo@g-com.eu>
2019-09-26 20:28:23 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Policykit (Agent)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-policykit-agent/de/

--- translations/lxqt-policykit-agent_de.ts
+++ translations/lxqt-policykit-agent_de.ts
@@ -12,7 +12,7 @@
     <message>
         <location filename="../src/policykitagent.cpp" line="75"/>
         <source>Another authentication is in progress. Please try again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Eine andere Authentifizierung läuft derzeit. Bitte versuchen Sie es später erneut.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="145"/>
