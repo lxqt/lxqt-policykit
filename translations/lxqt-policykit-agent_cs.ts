5a2b6a5a018eed1f84081c0367ab11c9101c1247
p-bo <pavel.borecki@gmail.com>
2019-09-27 06:02:42 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Policykit (Agent)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-policykit-agent/cs/

--- translations/lxqt-policykit-agent_cs.ts
+++ translations/lxqt-policykit-agent_cs.ts
@@ -12,7 +12,7 @@
     <message>
         <location filename="../src/policykitagent.cpp" line="75"/>
         <source>Another authentication is in progress. Please try again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Právě už probíhá jiné ověřování se. Zkuste to prosím později.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="145"/>
