7e74ae6f0697b11c04b06083707873a0281b7f46
Moo <hazap@hotmail.com>
2019-09-27 10:08:23 +0000

    Translated using Weblate (Lithuanian)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Policykit (Agent)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-policykit-agent/lt/

--- translations/lxqt-policykit-agent_lt.ts
+++ translations/lxqt-policykit-agent_lt.ts
@@ -12,7 +12,7 @@
     <message>
         <location filename="../src/policykitagent.cpp" line="75"/>
         <source>Another authentication is in progress. Please try again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vyksta kitas tapatybės nustatymas. Vėliau bandykite dar kartą.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="145"/>
