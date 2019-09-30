f478034e311a2ccd1013a0b69ea661395ce07874
scootergrisen <scootergrisen@gmail.com>
2019-09-30 21:34:18 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Policykit (Agent)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-policykit-agent/da/

--- translations/lxqt-policykit-agent_da.ts
+++ translations/lxqt-policykit-agent_da.ts
@@ -12,7 +12,7 @@
     <message>
         <location filename="../src/policykitagent.cpp" line="75"/>
         <source>Another authentication is in progress. Please try again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>En anden godkendelse er i gang. Prøv venligst igen senere.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="145"/>
