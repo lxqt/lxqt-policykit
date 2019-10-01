76fc6249745f6d472d4661ebe06376fb977dd598
Ayhan YALÇINSOY PhD. (Dr.) <ayhanyalcinsoy@pisilinux.org>
2019-10-01 19:34:59 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Policykit (Agent)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-policykit-agent/tr/

--- translations/lxqt-policykit-agent_tr.ts
+++ translations/lxqt-policykit-agent_tr.ts
@@ -12,7 +12,7 @@
     <message>
         <location filename="../src/policykitagent.cpp" line="75"/>
         <source>Another authentication is in progress. Please try again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Başka bir kimlik doğrulama işlemi devam ediyor. Lütfen daha sonra yeniden deneyiniz.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="145"/>
