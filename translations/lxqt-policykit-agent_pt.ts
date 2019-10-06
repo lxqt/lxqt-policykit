4bacc773ca066b893359747156f3ab4490cc0cde
Sérgio Marques <smarquespt@gmail.com>
2019-10-06 22:38:52 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Policykit (Agent)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-policykit-agent/pt/

--- translations/lxqt-policykit-agent_pt.ts
+++ translations/lxqt-policykit-agent_pt.ts
@@ -12,7 +12,7 @@
     <message>
         <location filename="../src/policykitagent.cpp" line="75"/>
         <source>Another authentication is in progress. Please try again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Já está em curso uma autenticação. Por favor tente mais tarde.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="145"/>
