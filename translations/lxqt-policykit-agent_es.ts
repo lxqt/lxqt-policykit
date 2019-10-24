3073d23195872afca7199c0f1143a31bc45e6d2d
micrococo <micrococo@gmx.com>
2019-10-24 17:12:14 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Policykit (Agent)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-policykit-agent/es/

--- translations/lxqt-policykit-agent_es.ts
+++ translations/lxqt-policykit-agent_es.ts
@@ -12,7 +12,7 @@
     <message>
         <location filename="../src/policykitagent.cpp" line="75"/>
         <source>Another authentication is in progress. Please try again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Otra autentificación está en progreso. Inténtelo de nuevo más tarde.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="145"/>
