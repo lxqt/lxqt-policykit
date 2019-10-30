9c07f446467ddbd47c1854bcb3e7b37a76c35ab1
Marcin Mikołajczak <marcin123407@vivaldi.net>
2019-10-30 11:25:29 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Policykit (Agent)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-policykit-agent/pl/

--- translations/lxqt-policykit-agent_pl.ts
+++ translations/lxqt-policykit-agent_pl.ts
@@ -12,7 +12,7 @@
     <message>
         <location filename="../src/policykitagent.cpp" line="75"/>
         <source>Another authentication is in progress. Please try again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Trwa inne uwierzytelnianie. Spróbuj ponownie później.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="145"/>
