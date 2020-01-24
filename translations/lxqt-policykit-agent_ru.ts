890fea5430ec2395d9e01053207a4797da3cbd10
Golubev Alexander <fatzer2@gmail.com>
2020-01-24 14:15:50 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Policykit (Agent)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-policykit-agent/ru/

--- translations/lxqt-policykit-agent_ru.ts
+++ translations/lxqt-policykit-agent_ru.ts
@@ -12,7 +12,7 @@
     <message>
         <location filename="../src/policykitagent.cpp" line="75"/>
         <source>Another authentication is in progress. Please try again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>В данный момент выполняется другая попытка аутентификации. Попробуйте повторить позже.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="145"/>
