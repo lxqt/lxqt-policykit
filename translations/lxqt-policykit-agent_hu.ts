e6882d4a41a8f06a583b946351251c753c5caa93
Ács Zoltán <acszoltan111@gmail.com>
2020-02-06 14:36:27 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: LXQt/LXQt Policykit (Agent)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-policykit-agent/hu/

--- translations/lxqt-policykit-agent_hu.ts
+++ translations/lxqt-policykit-agent_hu.ts
@@ -12,7 +12,7 @@
     <message>
         <location filename="../src/policykitagent.cpp" line="75"/>
         <source>Another authentication is in progress. Please try again later.</source>
-        <translation type="unfinished">Egy másik hitelesítés van folyamatban. Próbálkozzon később.</translation>
+        <translation>Egy másik hitelesítés van folyamatban. Próbálkozzon később.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="145"/>
