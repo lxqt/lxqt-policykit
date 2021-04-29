f631bb2585d6bfa3d7347b355f8aff1c87a7b982
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-04-29 20:07:17 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/policy-kit-module-name/et/
    Translation: LXQt Configuration/lxqt-policykit
    Translation: LXQt Configuration/policy-kit Module Name

---
+++ translations/lxqt-policykit-agent_et.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="75"/>
+        <location filename="../src/policykitagent.cpp" line="169"/>
+        <source>PolicyKit Information</source>
+        <translation>PolicyKit&apos;i teave</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="75"/>
+        <source>Another authentication is in progress. Please try again later.</source>
+        <translation>Teine autentimine on pooleli. Palun proovi hiljem uuesti.</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="145"/>
+        <source>Authorization Failed</source>
+        <translation>Autentimine ei õnnestunud</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="145"/>
+        <source>Authorization failed for some reason</source>
+        <translation>Mingil põhjusel autentimine ei õnnestunud</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="162"/>
+        <source>PolicyKit Error</source>
+        <translation>PolicyKit&apos;i viga</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>Autentimine on vajalik</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>Kasutaja:</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>Salasõna:</translation>
+    </message>
+</context>
+</TS>
