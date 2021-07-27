baf058601c4bb577aab4a534a9449e4a798df8d5
Sergiu Bivol <sergiu@cip.md>
2021-07-27 20:07:47 +0200

    Weblate commits
    Co-authored-by: Sergiu Bivol <sergiu@cip.md>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/ro/
    Translation: LXQt Configuration/lxqt-policykit

---
+++ translations/lxqt-policykit-agent_ro.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ro">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="75"/>
+        <location filename="../src/policykitagent.cpp" line="169"/>
+        <source>PolicyKit Information</source>
+        <translation>Informații PolicyKit</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="75"/>
+        <source>Another authentication is in progress. Please try again later.</source>
+        <translation>Altă autentificare e în curs. Reîncercați mai târziu.</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="145"/>
+        <source>Authorization Failed</source>
+        <translation>Autorizare eșuată</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="145"/>
+        <source>Authorization failed for some reason</source>
+        <translation>Autorizarea a eșuat dintr-un motiv oarecare</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="162"/>
+        <source>PolicyKit Error</source>
+        <translation>Eroare PolicyKit</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>Se cere autentificarea</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>Identitate:</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>Parolă:</translation>
+    </message>
+</context>
+</TS>
