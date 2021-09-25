8a869cbe51753d17c25859536ff73df9163e9a28
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-09-26 03:24:47 +0800

    Weblate commits (#120)
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/policy-kit-module-name/ko/
    Translation: LXQt Configuration/lxqt-policykit
    Translation: LXQt Configuration/policy-kit Module Name

---
+++ translations/lxqt-policykit-agent_ko.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="75"/>
+        <location filename="../src/policykitagent.cpp" line="169"/>
+        <source>PolicyKit Information</source>
+        <translation>PolicyKit 정보</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="75"/>
+        <source>Another authentication is in progress. Please try again later.</source>
+        <translation>다른 인증이 진행 중입니다. 나중에 다시 시도하십시오.</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="145"/>
+        <source>Authorization Failed</source>
+        <translation>인증 실패</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="145"/>
+        <source>Authorization failed for some reason</source>
+        <translation>몇몇 이유로 인해 인증에 실패했습니다</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="162"/>
+        <source>PolicyKit Error</source>
+        <translation>PolicyKit 오류</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>인증 필요</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>ID:</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>비밀번호:</translation>
+    </message>
+</context>
+</TS>
