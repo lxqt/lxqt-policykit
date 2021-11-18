a3d93ac1d69840e114d5231f9fdac1ffce68aa2c
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-11-18 18:33:05 +0100

    Weblate commits (#123)
    Co-authored-by: Joan Miró <miro.oro.jm@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/ca/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/policy-kit-module-name/ca/
    Translation: LXQt Configuration/lxqt-policykit
    Translation: LXQt Configuration/policy-kit Module Name

--- translations/lxqt-policykit-agent_ca.ts
+++ translations/lxqt-policykit-agent_ca.ts
@@ -12,7 +12,7 @@
     <message>
         <location filename="../src/policykitagent.cpp" line="75"/>
         <source>Another authentication is in progress. Please try again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hi ha una altra autenticació en curs. Torneu-ho a provar més tard.</translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="145"/>
