ad33319dec1f26cdc70f8aceb76da29f687fbdd8
Quentin PAGÈS <quentinantonin@free.fr>
2023-01-25 07:59:53 +0100

    Weblate commits
    Co-authored-by: Quentin PAGÈS <quentinantonin@free.fr>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/oc/
    Translation: LXQt Configuration/lxqt-policykit

---
+++ translations/lxqt-policykit-agent_oc.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="oc">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="81"/>
+        <location filename="../src/policykitagent.cpp" line="174"/>
+        <source>PolicyKit Information</source>
+        <translation>Informacions de PolicyKit</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="81"/>
+        <source>Another authentication is in progress. Please try again later.</source>
+        <translation>Una autra autentificacion es en cor. Tornatz ensajar mai tard.</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="151"/>
+        <source>Authorization Failed</source>
+        <translation>Fracàs de l’autorizacion</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="151"/>
+        <source>Authorization failed for some reason</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="167"/>
+        <source>PolicyKit Error</source>
+        <translation>Error de PolicyKit</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>Autentificacion requerida</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>Identitat :</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>Senhal :</translation>
+    </message>
+</context>
+</TS>
