076d822c94afa8ca03613e48a9becf2538373e05
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-05-15 12:40:14 +0200

    Weblate commits (#134)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/fi/
    Translation: LXQt Configuration/lxqt-policykit
    
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>

---
+++ translations/lxqt-policykit-agent_fi.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="81"/>
+        <location filename="../src/policykitagent.cpp" line="174"/>
+        <source>PolicyKit Information</source>
+        <translation>PolicyKit-tiedot</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="81"/>
+        <source>Another authentication is in progress. Please try again later.</source>
+        <translation>Toinen tunnistautuminen on käynnissä. Yritä myöhemmin uudelleen.</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="151"/>
+        <source>Authorization Failed</source>
+        <translation>Tunnistautuminen epäonnistui</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="151"/>
+        <source>Authorization failed for some reason</source>
+        <translation>Tunnistautuminen epäonnistui jostakin syystä</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="167"/>
+        <source>PolicyKit Error</source>
+        <translation>PolicyKit-virhe</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>Tunnistautumista vaaditaan</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>Identiteetti:</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>Salasana:</translation>
+    </message>
+</context>
+</TS>
