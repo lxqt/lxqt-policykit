1d61190132572df39c7c929b97b91fc4e7fdd47f
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-12-25 08:53:39 +0100

    Weblate commits (#140)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/pa/
    Translation: LXQt Configuration/lxqt-policykit
    
    Co-authored-by: A S Alam <amanpreet.alam@gmail.com>

---
+++ translations/lxqt-policykit-agent_pa.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pa">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="85"/>
+        <location filename="../src/policykitagent.cpp" line="183"/>
+        <source>PolicyKit Information</source>
+        <translation>ਪਾਲਸੀਕਿੱਟ ਜਾਣਕਾਰੀ</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="82"/>
+        <source>Another authentication is in progress. Please try again later.</source>
+        <translation>ਹੋਰ ਪਰਮਾਣਕਿਤਾ ਜਾਰੀ ਹੈ। ਬਾਅਦ ਵਿੱਚ ਫੇਰ ਕੋਸ਼ਿਸ਼ ਕਰੋ।</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="160"/>
+        <source>Authorization Failed</source>
+        <translation>ਪਰਮਾਣਕਿਤਾ ਫੇਲ੍ਹ ਹੈ</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="160"/>
+        <source>Authorization failed for some reason</source>
+        <translation>ਕੁਝ ਕਾਰਨ ਕਰਕੇ ਪਰਮਾਣਕਿਤਾ ਫੇਲ੍ਹ ਹੈ</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="176"/>
+        <source>PolicyKit Error</source>
+        <translation>ਪਾਲਸੀਕਿਟ ਗਲਤੀ</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>ਪਰਮਾਣਕਿਤਾ ਚਾਹੀਦੀ ਹੈ</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>ਪਛਾਣ:</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>ਪਾਸਵਰਡ:</translation>
+    </message>
+</context>
+</TS>
