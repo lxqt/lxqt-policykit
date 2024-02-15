a09d1cedc17705622d50c2cab81b46d180b72924
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-02-15 08:11:02 +0100

    Weblate commits (#142)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/lg/
    Translation: LXQt Configuration/lxqt-policykit
    
    Co-authored-by: Kizito Birabwa <kbirabwa@yahoo.co.uk>

---
+++ translations/lxqt-policykit-agent_lg.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lg">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="85"/>
+        <location filename="../src/policykitagent.cpp" line="183"/>
+        <source>PolicyKit Information</source>
+        <translation>Ebifa ku PolicyKit</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="82"/>
+        <source>Another authentication is in progress. Please try again later.</source>
+        <translation>Waliwo okukakasibwa okulala okubumbujja. Gezako eddako.</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="160"/>
+        <source>Authorization Failed</source>
+        <translation>Okuwa buyinza kugaanye</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="160"/>
+        <source>Authorization failed for some reason</source>
+        <translation>Waliwo ekiremeseza okuwa buyinza</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="176"/>
+        <source>PolicyKit Error</source>
+        <translation>PolicyKit ezzeko kiremya</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>Kyetaagisa kukakasa</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>Obumanye:</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="65"/>
+        <source>Password:</source>
+        <translation>Kasumuluzo:</translation>
+    </message>
+</context>
+</TS>
