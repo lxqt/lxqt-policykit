4855c5601b832f47e8abaff34b35c0a618aabe64
Weblate <noreply@weblate.org>
2025-01-02 15:15:33 +0100

    Weblate commits
    Co-authored-by: Andi Chandler <andi@gowling.com>
    Co-authored-by: ButterflyOfFire <butterflyoffire@protonmail.com>
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/kab/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/policy-kit-module-name/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/policy-kit-module-name/ka/
    Translation: LXQt Configuration/lxqt-policykit
    Translation: LXQt Configuration/policy-kit Module Name

---
+++ translations/lxqt-policykit-agent_ka.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="85"/>
+        <location filename="../src/policykitagent.cpp" line="183"/>
+        <source>PolicyKit Information</source>
+        <translation>PolicyKit-ის ინფორმაცია</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="82"/>
+        <source>Another authentication is in progress. Please try again later.</source>
+        <translation>მიმდინარეობს სხვა ავთენტიკაცია. მოგვიანებით სცადეთ.</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="160"/>
+        <source>Authorization Failed</source>
+        <translation>ავტორიზაცია ჩავარდა</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="160"/>
+        <source>Authorization failed for some reason</source>
+        <translation>ავტორიზაცია რატომღაც ჩავარდა</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="176"/>
+        <source>PolicyKit Error</source>
+        <translation>PolicyKit-ის შეცდომა</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>საჭიროა ავთენტიკაცია</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>იდენტიფიკატორი:</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="65"/>
+        <location filename="../src/policykitagentgui.cpp" line="74"/>
+        <source>Password:</source>
+        <translation>პაროლი:</translation>
+    </message>
+</context>
+</TS>
