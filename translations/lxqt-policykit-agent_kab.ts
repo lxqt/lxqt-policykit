e0a77eca5beae6846b15a65b40d1182ff608d20e
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-01-02 15:16:05 +0100

    Weblate commits (#150)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/kab/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/policy-kit-module-name/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/policy-kit-module-name/ka/
    Translation: LXQt Configuration/lxqt-policykit
    Translation: LXQt Configuration/policy-kit Module Name
    
    Co-authored-by: Andi Chandler <andi@gowling.com>
    Co-authored-by: ButterflyOfFire <butterflyoffire@protonmail.com>
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>

---
+++ translations/lxqt-policykit-agent_kab.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="kab">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="85"/>
+        <location filename="../src/policykitagent.cpp" line="183"/>
+        <source>PolicyKit Information</source>
+        <translation>Talɣut ɣef PolicyKit</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="82"/>
+        <source>Another authentication is in progress. Please try again later.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="160"/>
+        <source>Authorization Failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="160"/>
+        <source>Authorization failed for some reason</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="176"/>
+        <source>PolicyKit Error</source>
+        <translation>Tuccḍa n PolicyKit</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="65"/>
+        <location filename="../src/policykitagentgui.cpp" line="74"/>
+        <source>Password:</source>
+        <translation>Awal n uɛeddi:</translation>
+    </message>
+</context>
+</TS>
