9cd7ff1440884d103eff30248d41b21744d0ed8c
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-02-11 14:39:12 +0100

    Weblate commits (#152)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/zh_TW/
    Translation: LXQt Configuration/lxqt-policykit
    
    Co-authored-by: BigELK176 ≡ <BigELK176@gmail.com>

---
+++ translations/lxqt-policykit-agent_zh_TW.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>LXQtPolicykit::PolicykitAgent</name>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="85"/>
+        <location filename="../src/policykitagent.cpp" line="183"/>
+        <source>PolicyKit Information</source>
+        <translation>PolicyKit 資訊</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="82"/>
+        <source>Another authentication is in progress. Please try again later.</source>
+        <translation>另有授權正在進行。請稍後再試。</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="160"/>
+        <source>Authorization Failed</source>
+        <translation>授權失敗</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="160"/>
+        <source>Authorization failed for some reason</source>
+        <translation>有些原造成授權失敗</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagent.cpp" line="176"/>
+        <source>PolicyKit Error</source>
+        <translation>PolicyKit 錯誤</translation>
+    </message>
+</context>
+<context>
+    <name>PolicykitAgentGUI</name>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="14"/>
+        <source>Authentication Required</source>
+        <translation>需要授權</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="55"/>
+        <source>Identity:</source>
+        <translation>身分：</translation>
+    </message>
+    <message>
+        <location filename="../src/policykitagentgui.ui" line="65"/>
+        <location filename="../src/policykitagentgui.cpp" line="74"/>
+        <source>Password:</source>
+        <translation>密碼：</translation>
+    </message>
+</context>
+</TS>
