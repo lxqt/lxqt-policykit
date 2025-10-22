1f33a2da187f4c0ce418b7e4539e3e3e2eb00587
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-10-22 21:21:00 +0200

    Weblate commits (#156)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-policykit/pt/
    Translation: LXQt Configuration/lxqt-policykit
    
    Co-authored-by: Roberalz <lumintnian@outlook.com>
    Co-authored-by: virtual-hand <handvirtual@tutamail.com>

--- translations/lxqt-policykit-agent_eu.ts
+++ translations/lxqt-policykit-agent_eu.ts
@@ -1,33 +1,33 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pt">
+<TS version="2.1" language="eu">
 <context>
     <name>LXQtPolicykit::PolicykitAgent</name>
     <message>
         <location filename="../src/policykitagent.cpp" line="85"/>
         <location filename="../src/policykitagent.cpp" line="183"/>
         <source>PolicyKit Information</source>
-        <translation>Informações do PolicyKit</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="82"/>
         <source>Another authentication is in progress. Please try again later.</source>
-        <translation>Já está em curso uma autenticação. Por favor tente mais tarde.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="160"/>
         <source>Authorization Failed</source>
-        <translation>Falha de autorização</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="160"/>
         <source>Authorization failed for some reason</source>
-        <translation>Autorização falhou por algum motivo</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/policykitagent.cpp" line="176"/>
         <source>PolicyKit Error</source>
-        <translation>Erro PolicyKit</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -35,18 +35,18 @@
     <message>
         <location filename="../src/policykitagentgui.ui" line="14"/>
         <source>Authentication Required</source>
-        <translation>Requer autenticação</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/policykitagentgui.ui" line="55"/>
         <source>Identity:</source>
-        <translation>Identidade:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/policykitagentgui.ui" line="65"/>
         <location filename="../src/policykitagentgui.cpp" line="74"/>
         <source>Password:</source>
-        <translation>Senha:</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
